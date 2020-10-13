587bf938a44dab38c6b027cf1daf6541bd6f9541
riinuots <riinuots@gmail.com>
2020-10-13 17:29:33 +0100

    added specialty and period

--- 02_check_dashboard.R
+++ 02_check_dashboard.R
@@ -2,6 +2,7 @@
 
 library(tidyverse)
 library(RCurl)
+library(readxl)
 periods_orig = postForm(uri='https://globalsurgery.redcap.bham.ac.uk/api/',
                         token=Sys.getenv("surgweek_teams"),
                         content = 'record',
@@ -11,6 +12,32 @@ periods_orig = postForm(uri='https://globalsurgery.redcap.bham.ac.uk/api/',
                         exportCheckboxLabel='false',
                         returnFormat = 'json') %>% 
     read_csv()
+
+# Adding specialities and periods to the dashboard:
+specialtyperiods_orig = postForm(uri='https://globalsurgery.redcap.bham.ac.uk/api/',
+                                 token=Sys.getenv("surgweek_teams"),
+                                 content='report',
+                                 format='csv',
+                                 report_id='252',
+                                 rawOrLabel='raw',
+                                 rawOrLabelHeaders='raw',
+                                 exportCheckboxLabel='false',
+                                 returnFormat = 'json') %>% 
+    read_csv(col_types = cols(.default = col_character())) %>% 
+    mutate(record_id = parse_number(record_id)) %>% 
+    rename(specialty_id = specialty)
+
+specialty_abbreviation <- read_excel("specialty abbreviation.xlsx", 
+                                     col_types = c("text", "skip", "text"))
+
+specialtyperiods  = specialtyperiods_orig %>% 
+    separate_rows(specialty_id) %>% 
+    filter_at(vars(all_of(c("specialty_id", "period"))), any_vars(! is.na(.))) %>% 
+    left_join(specialty_abbreviation) %>% 
+    select(-specialty_id) %>% 
+    group_by(record_id, period) %>% 
+    summarise(specialty = str_c(specialty, collapse = "-")) %>% 
+    rename(Period = period, Specialty = specialty)
 
 periods_check = periods_orig %>%
     mutate(
@@ -37,15 +64,20 @@ periods_check = periods_orig %>%
         accounts_created_success = case_when(accounts_created == "Accounts created" ~ "Yes", 
                                              emails_unique_success == "Yes" ~ "Not yet",
                                              TRUE ~ NA_character_)) %>%
-    select(
-        record_id,
+    select(record_id,
         time,
         form_complete,
         period_choice_success,
         orcids_valid_success,
         duplicate_check_success,
         emails_unique_success,
-        accounts_created_success)
+        accounts_created_success) %>% 
+    left_join(specialtyperiods)
+
+stopifnot(nrow(periods_orig) == nrow(periods_check))
+
+
+
 
 #save(periods_check, file = "periods_check.rda") 
 
